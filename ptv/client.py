fc3aad49d4b533e602890fc7d3b6c6fe19939913
Leo <leoterray@yahoo.com.au>
2019-06-21 11:00:58 +1000

    Added departures functions

--- ptv/client.py
+++ ptv/client.py
@@ -4,7 +4,6 @@ import hmac
 import requests
 import urllib
 
-API_VER = '/v3/'
 BASE_URL = 'http://timetableapi.ptv.vic.gov.au'
 
 class PTVClient(object):
@@ -62,7 +61,7 @@ class PTVClient(object):
         url = BASE_URL + path + query + "$signature=" + self._calculateSignature(path + query)
         return url
         
-    def _apiCall(self, path, params = {}): 
+    def _callApi(self, path, params = {}): 
         """
         Calls API
 
@@ -81,4 +80,64 @@ class PTVClient(object):
         response = requests.get(self._getUrl(path, params))
         response.raise_for_status()
         return response.json()
+    
+    def get_departures_from_stop(self, route_type, stop_id, route_id=None, platform_numbers=None, direction_id=None, direction_id=None, look_backwards=None, gtfs=None, date_utc=None, max_results=None, include_cancelled = None, expand = None):
+        """
+        View departures from a stop
+
+        Parameters
+        ----------
+        route_type : integer
+            Number identifying transport mode; values returned via RouteTypes API
+        stop_id : integer
+            Identifier of stop; values returned by Stops API
+        
+        Optional Parameters
+        -------------------
+        route_id : string
+            Identifier of route; values returned by RoutesAPI - v3/routes
+        platform_numbers : Array[integer]
+            Filter by platform number at stop
+        direction_id : integer
+            Filter by indentifier of direction of travel; values returned by Directions Api - /v3/directions/route/{route_id}
+        look_backwards : boolean
+            Indicates if filtering runs (and their departures) to those that arrive at destination before date_utc (default = false). Requires max_results > 0.
+        gtfs : boolean
+            Indicates that stop_id parameter will accept "GTFS stop_id" data
+        date_utc : string
+            Filter by the date and time of the request (ISO 8601 UTC format) (default = current date and time)
+        max_results : integer
+            Maximum number of results returned
+        include_cancelled : boolean
+            Indicates if cancelled services (if they exist) are returned (default = false) - metropolitan train only
+        expand : Array[string]
+            List objects to be returned in full (i.e. expanded) - options include: all, stop, route, run, direction, disruption
+
+        Returns
+        -------
+        Departures : dict
+            Dictionary of departures
+        """
+        path = "/v3/departures/route_type/{}/stop/{}"
+        path = path.format(route_type,stop_id)
+        params = {}
+        if route_id:
+            path += f"/route/{route_id}"
+        if platform_numbers:
+            params['platform_numbers'] = platform_numbers
+        if direction_id:
+            params['direction_id'] = direction_id
+        if look_backwards:
+            params['look_backwards'] = look_backwards
+        if gtfs:
+            params['gtfs'] = str(gtfs).lower()
+        if date_utc:
+            params['date_utc'] = date_utc
+        if max_results:
+            params['max_results'] = max_results
+        if include_cancelled:
+            params['include_cancelled'] = str(include_cancelled).lower()
+        if expand:
+            params['expand'] = str(expand).lower()
+        return self._callApi(path, params)
         
